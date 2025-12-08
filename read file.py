da9ad11ef3c607dd7a83d2d0c125c8e51e7e8f0d
automataTY <2924450788@qq.com>
2025-12-08 14:47:17 +0800

    Update read file.py

--- read file.py
+++ read file.py
@@ -1,10 +1,23 @@
 import json as js
-with open('1709823572.json',encoding = 'utf-8') as file:
-	get_content = js.load(file)
-	#print(get_content)
-note_read = get_content['note'];
-print(type(note_read))
-print(note_read[0])
+import os
+import numpy as py
+root = os.getcwd()
+path = os.listdir(root)
+file_play = list()
+for p in path:
+	type_file = os.path.splitext(p)
+	#print(type_file[1])
+	if(type_file[1] == '.json'):
+		file_play.append(p)
+for i in range(0,len(file_play),1):
+	use_file = file_play[i]
+	with open(use_file,'r',encoding = 'utf-8') as file:
+		get_content = js.load(file)
+		#print(get_content)
+	meta_read = get_content['meta']
+	note_read = get_content['note']
+	print(meta_read,'\n')
+	print(note_read)
 
 
 
