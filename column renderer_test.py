13b4561f49fd9b66bb3bddbdc5dc0429de9e5934
automataTY <2924450788@qq.com>
2025-12-11 12:28:11 +0800

    Update column renderer_test.py

--- column renderer_test.py
+++ column renderer_test.py
@@ -4,21 +4,7 @@ import os
 import numpy as py
 import pygame as pg
 
-def note_judge(note_file,note_wait,bar_count,sp,length):
-	if(sp < length-1):
-		for i in range(0,4,1):
-			tail_note = note_file[sp]
-			tail_note_bar = tail_note['beat'][0]
-			if(tail_note_bar == bar_count+2):
-				note_wait.append(tail_note)
-				sp++
-			elif(tail_note_bar > bar_count+2): break
-		return sp
-	elif(sp == length-1): return -1
-
-def note_draw(column,note_bar,note_bar_div,bar_count,present_time,screen):
-	
-def note_load(note_file,bpm,screen):
+def note_load(note_file,bpm,screen,clock):
 	length = len(note_file)
 	bar_delta = 60/bpm
 	sp = 0
@@ -26,15 +12,51 @@ def note_load(note_file,bpm,screen):
 		present_time = pg.time.get_ticks()/1000
 		bar_count = int(present_time/bar_delta)
 		note_wait = list();
-		sp = note_buffer(note_file,note_wait,bar_count,sp,length)
+		sp = note_judge(note_file,note_wait,bar_count,sp,length)
 		for i in range(0,len(note_wait),1):
 			column = note_wait[i]['column']
-			note_bar = note_wait[i]['beat'][0]
+			#note_bar = note_wait[i]['beat'][0]
 			note_bar_div = note_wait[i]['beat'][1]/note_wait[i]['beat'][2]
-			note_draw(colunm,note_bar,note_bar_div,bar_count,present_time,screen)
+			note_draw(column,screen)
+			clock.tick(bar_delta*note_bar_div)
 
+def note_judge(note_file,note_wait,bar_count,sp,length):
+	if(sp < length-1):
+		for i in range(0,4,1):
+			tail_note = note_file[sp]
+			tail_note_bar = tail_note['beat'][0]
+			if(tail_note_bar == bar_count+2):
+				note_wait.append(tail_note)
+				sp += 1
+			elif(tail_note_bar > bar_count+2): break
+		return sp
+	elif(sp == length-1): return -1
 
-
+def note_draw(column,screen):
+	s_width = pg.Surface.get_width(screen)
+	s_height = pg.Surface.get_height(screen)
+	rect = pg.Rect(0,0,50,10)
+	if(column == 0):
+		rect.center = (s_width/2-3*25,0)
+		while(rect.y > s_height):
+			rect.y += s_height/10
+			pygame.draw.rect(screen,'white',rect, 0)
+	elif(column == 1):
+		rect.center = (s_width/2-1*25,0)
+		while(rect.y > s_height):
+			rect.y += s_height/10
+			pygame.draw.rect(screen,'white',rect, 0)
+	elif(column == 2):
+		rect.center = (s_width/2+1*25,0)
+		while(rect.y > s_height):
+			rect.y += s_height/10
+			pygame.draw.rect(screen,'white',rect, 0)
+	elif(column == 3):
+		rect.center = (s_width/2+3*25,0)
+		while(rect.y > s_height):
+			rect.y += s_height/10
+			pygame.draw.rect(screen,'white',rect, 0)
+	
 pg.init()
 root = os.getcwd()
 path = os.listdir(root)
@@ -61,6 +83,7 @@ while isRunning:
 		if(ev.type == pg.QUIT): #保证点右上角的x退出时不会卡死
 			isRunning = False
 			break
+		else: note_load(note,bpm,screen,clock)
 	pg.display.update() #更新屏幕
 	clock.tick(60) #两次循环间隔(等价于60帧,保证按键有不响应期)
 
